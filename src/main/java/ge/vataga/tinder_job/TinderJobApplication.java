6c79bc2abbc6354f5a978dd9c0382d4f98430c10
Lepeshinsky <s.lepeshinskiy@petshop.ru>
2023-08-27 10:57:32 +0300

    Initial commit

---
+++ src/main/java/ge/vataga/tinder_job/TinderJobApplication.java
@@ -0,0 +1,13 @@
+package ge.vataga.tinder_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/vataga/tinder_job/TinderJobApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.vataga.tinder_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
