049c7c09d08160abd333973700b86ac61debc7e8
Mikhail Knyazev <solnseselo@yandex.ru>
2023-08-27 15:02:16 +0400

    add dirt

--- src/main/java/ge/vataga/tinder_job/controller/UserController.java
+++ src/main/java/ge/vataga/tinder_job/controller/UserController.java
@@ -1,7 +1,36 @@
 package ge.vataga.tinder_job.controller;
 
+import ge.vataga.tinder_job.entity.User;
+import ge.vataga.tinder_job.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/get")
+    public ResponseEntity<User> getUser(@RequestParam Integer userId) {
+        User userById = userService.getUserById(userId);
+        return ResponseEntity.ok(userById); // Создайте HTML-шаблон с формой добавления пользователя (например, add-user.html)
+    }
+
+    // Обработка отправки формы для добавления пользователя
+    @PostMapping("/add")
+    public ResponseEntity<String> addUser(@RequestBody User user) {
+        // Вызываем сервис для добавления пользователя
+        userService.createUser(user.getUsername(), user.getPassword(), user.getName(), user.getRole(), user.getSkills());
+
+        // Перенаправляем пользователя на страницу успешного добавления
+        return ResponseEntity.ok("success");
+    }
 }
--- src/main/java/ge/vataga/tinder_job/service/UserService.java
+++ src/main/java/ge/vataga/tinder_job/service/UserService.java
@@ -1,7 +1,55 @@
 package ge.vataga.tinder_job.service;
 
+import ge.vataga.tinder_job.entity.User;
+import ge.vataga.tinder_job.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User createUser(String username, String password, String name, String role, String skills) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setName(name);
+        user.setRole(role);
+        user.setSkills(skills);
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Integer userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public User updateUser(Integer userId, String name, String role, String skills) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            user.setName(name);
+            user.setRole(role);
+            user.setSkills(skills);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    @Transactional
+    public void deleteUser(Integer userId) {
+        userRepository.deleteById(userId);
+    }
 }
